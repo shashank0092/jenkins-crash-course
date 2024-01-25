869b80ce98e1dbfbee97d45c2715dd43d02a783f
SHASHANK <shashank7016204503@gmail.com>
2024-01-25 11:31:36 +0530

    Second Commit

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("THIS IS JAVA PROGRAM FOR JENKINS");
         System.out.println("OUTPUT BOI");
+        System.out.println("YOU ARE A SHUKLA BOI");
     }
 }
